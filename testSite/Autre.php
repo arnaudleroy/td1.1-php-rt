e9892bb76522bac81dbd3426170d5c3a5f8033c9
Arnaud LEROY <Arnaud LEROY>
2017-03-02 18:34:47 +0100

    fin

---
+++ testSite/Autre.php
@@ -0,0 +1,20 @@
+<DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<form>
+	<input type="number" name="a">
+	<input type="number" name="b">
+	<input type="submit" value="Ajouter">
+</form>
+
+<?php
+if(isset($_GET["a"]) && isset($_GET["b"])){
+	$resultat=$_GET["a"]+$_GET["b"];
+	echo "La somme est $resultat";
+}
+?>
+</body>
+</html>
